9893fa69dbe4207c2b58c1907de2a3a7fbd876b3
T095T <72501211+T095T@users.noreply.github.com>
2023-07-22 05:52:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,62 +1,23 @@
-// Create web server
-
+//Create web Server
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
-var http = require('http');
-
-// connect to database
-const db = require('../database/index.js');
-
-// middleware
+const port = 3000;
 const bodyParser = require('body-parser');
+
+//Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-// serve static files
-app.use(express.static(path.join(__dirname, '../client/dist')));
+//Set view engine to ejs
+app.set('view engine', 'ejs');
 
-// routes
-app.get('/comments', (req, res) => {
-  db.getComments((err, data) => {
-    if (err) {
-      console.log('error retrieving comments');
-      res.status(404).send();
-    } else {
-      console.log('retrieved comments');
-      res.status(200).send(data);
-    }
-  });
-});
+//Set static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/comments', (req, res) => {
-  db.postComment(req.body, (err) => {
-    if (err) {
-      console.log('error posting comment');
-      res.status(404).send();
-    } else {
-      console.log('posted comment');
-      res.status(201).send();
-    }
-  });
-});
+//Set routes
+app.use(require('./routes/index'));
+app.use(require('./routes/comments'));
+app.use(require('./routes/users'));
 
-app.delete('/comments', (req, res) => {
-  db.deleteComment(req.body, (err) => {
-    if (err) {
-      console.log('error deleting comment');
-      res.status(404).send();
-    } else {
-      console.log('deleted comment');
-      res.status(204).send();
-    }
-  });
-});
-
-// create a webserver using http
-var server  = http.createServer(app);
-server.listen(PORT);
-
-// app listen also rises a problem
-// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+//Start server
+app.listen(port, () => console.log(`Server stated on port ${port}`));
